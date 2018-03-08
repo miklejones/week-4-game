25eec67ca8dbc89a3cb78568b6cd58a5b1b280de
Michael Garland <michaelcgarland@gmail.com>
2018-03-08 02:12:19 -0500

    working project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 var randomGoal = 0;
 var wins = 0;
 var losses = 0;
-var yourScore = 0;
+var totalScore = 0;
 
 var redCrystal = 0;
 var blueCrystal = 0;
@@ -21,11 +21,60 @@ function crystalValues() {
     } while (greenCrystal === redCrystal || greenCrystal === blueCrystal || greenCrystal === yellowCrystal);;
 }
 
-function startGame() {
+function startRound() {
     randomGoal = Math.floor(Math.random() * 101) + 19;
     document.getElementById("target-score").innerHTML = randomGoal;
-
+    totalScore = 0;
+    document.getElementById("total-score").innerHTML = totalScore;
     crystalValues();
 }
+
+function startGame() {
+    startRound();
+    wins = 0;
+    losses = 0;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+}
+
+function checkScore() {
+    if (totalScore > randomGoal) {
+        losses ++;
+        document.getElementById("losses").innerHTML = losses;
+        startRound();
+    } else if (totalScore === randomGoal) {
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        startRound();
+    };
+}
+
+
+startGame();
+
+$("#red-gem").click(function () {
+    totalScore += redCrystal;
+    document.getElementById("total-score").innerHTML = totalScore;
+    checkScore();
+});
+
+$("#blue-gem").click(function () {
+    totalScore += blueCrystal;
+    document.getElementById("total-score").innerHTML = totalScore;
+    checkScore();
+});
+
+$("#yellow-gem").click(function () {
+    totalScore += yellowCrystal;
+    document.getElementById("total-score").innerHTML = totalScore;
+    checkScore();
+});
+
+$("#green-gem").click(function () {
+    totalScore += greenCrystal;
+    document.getElementById("total-score").innerHTML = totalScore;
+    checkScore();
+});
+
 
 
